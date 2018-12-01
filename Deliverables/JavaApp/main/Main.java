32378dc22d870058468280b46a270a5e15feb318
Joël Didier <studisys@protonmail.com>
2018-12-01 22:15:06 +0100

    Update Main Class

--- Deliverables/JavaApp/main/Main.java
+++ Deliverables/JavaApp/main/Main.java
@@ -3,7 +3,19 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 System.out.println("Hello !");
+		 System.out.println("Hello World ! Starting the fridge app...");
+		 
+		 
+		 // Subject to change !
+		 // =============================
+		 // Create the model
+		 Model model = new Model();
+		 
+		 // Create the view
+		 // View view = new View(model, model);
+		 
+		 // Create the controller
+		 // Controller controller = new Controller(view, model);
 	}
 
 }
